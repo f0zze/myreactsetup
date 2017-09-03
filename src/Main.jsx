90743f82085767ceef1b91e0487f57a7c2793231
Arthur Pumpa <arthur.pumpa@gmail.com>
2017-09-03 20:31:19 +0300

    add emotion

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
+import styled from 'emotion/react';
+import { ThemeProvider } from "theming";
+import { withState } from 'recompose';
+import { ellipsis } from "./emotion/mixins";
+import { H1, H2 } from "./emotion/typography";
+import Select from "./Select";
+import Ball from "./Ball";
 
-const Main = () => {
+const Description = styled(H2)`
+    composes: ${ellipsis};
+    width: 500px;
+    display: inline-block;
+`;
+
+const Main = (props) => {
     const Hello = `Home Page`;
     return (
-        <div>{Hello}</div>
+        <ThemeProvider theme={{primaryColor: props.color}}>
+            <div>
+                <Select value={props.color}
+                        onValueChange={props.setColor}
+                        options={['red', 'violet', 'black', 'green']}
+                />
+                <H1>{Hello}</H1>
+                <br/>
+                <Description color="grey">
+                    There sits the only king I mean to bend my knee to: the King in the North!
+                </Description>
+                <br/>
+
+                <Ball speed="2s"/>
+                <Ball speed="4s"/>
+                <Ball speed="3s"/>
+            </div>
+        </ThemeProvider>
     );
 };
 
-export default Main;
+Main.propTypes = {
+    color: React.PropTypes.string.isRequired,
+    setColor: React.PropTypes.func.isRequired
+};
+
+export default withState('color', 'setColor', 'violet')(Main);
 
 
--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Main';
 
-ReactDOM.render(<Main />, document.getElementById('app'));
+import './emotion/fonts';
+import './emotion/globals';
+
+ReactDOM.render(
+    <Main />,
+    document.getElementById('app'));
