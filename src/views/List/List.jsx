0fc71672d05556862e61675132da9b8c133d7fdf
Aigars B <a.beinarovics@gmail.com>
2017-09-11 23:01:08 +0300

    add decorator support and bindings for react-mobx

--- src/views/List/List.jsx
+++ src/views/List/List.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
+import { observer } from 'mobx-react';
 import { Row, Col } from 'react-flexbox-grid';
 import range from 'lodash/range';
 import ListItem from './ListItem';
 
 const listItems = range(5);
 
-const List = () => {
-    return (
-        <Row>
-            {listItems.map(item =>
-                <Col md={3}>
-                    <ListItem>
-                        {`Item ${item}`}
-                    </ListItem>
-                </Col>
-            )}
-        </Row>
-    );
-};
-
+@observer
+class List extends React.Component {
+    render() {
+        return (
+            <Row>
+                {listItems.map(item =>
+                    <Col md={3}>
+                        <ListItem>
+                            {`Item ${item}`}
+                        </ListItem>
+                    </Col>
+                )}
+            </Row>
+        );
+    }
+}
 export default List;
